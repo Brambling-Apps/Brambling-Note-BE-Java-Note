f0caf3949da6b0a71699cb4249275e5117190156
WordlessEcho <wordless@echo.moe>
2023-04-12 12:09:22 +0800

    Initital service
    - Register service to Consul.
    - Connect to PostgreSQL

---
+++ src/test/java/moe/echo/bramblingnote/note/NoteApplicationTests.java
@@ -0,0 +1,13 @@
+package moe.echo.bramblingnote.note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
